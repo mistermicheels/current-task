69ac9681c2c18b57a8cd333ce478efe49353adb9
mistermicheels <22038058+mistermicheels@users.noreply.github.com>
2020-10-08 23:03:10 +0200

    prevent running multiple instances of the app

--- src/main/main.js
+++ src/main/main.js
@@ -3,30 +3,36 @@ const { app, dialog } = require("electron");
 const Controller = require("./Controller");
 const Logger = require("./Logger");
 
-const logger = new Logger();
-logger.enableLoggingUnhandledErrorsAndRejections();
-logger.info("Starting application");
+const isFirstInstance = app.requestSingleInstanceLock();
 
-// prevent Electron from automatically showing errors as dialog boxes to the user
-// motivation: in very rare cases, the app runs into a (harmless) error on close
-// note that these kinds of errors are still being logged, see above
-dialog.showErrorBox = () => {};
+if (isFirstInstance) {
+    initializeApp();
+} else {
+    app.exit();
+}
 
-// this reduces visual artifacts when showing/hiding windows (like our custom dialogs) on Windows
-app.disableHardwareAcceleration();
+function initializeApp() {
+    const logger = new Logger();
+    logger.enableLoggingUnhandledErrorsAndRejections();
+    logger.info("Starting application");
 
-app.on("ready", () => {
+    // prevent Electron from automatically showing errors as dialog boxes to the user
+    // motivation: in very rare cases, the app runs into a (harmless) error on close
+    // note that these kinds of errors are still being logged, see above
+    dialog.showErrorBox = () => {};
+
+    // this reduces visual artifacts when showing/hiding windows (like our custom dialogs) on Windows
+    app.disableHardwareAcceleration();
+
     // this means Electron has finished initialization and we can use all APIs
-    onAppReady(logger);
-});
+    app.on("ready", async () => {
+        const controller = new Controller(logger);
 
-async function onAppReady(logger) {
-    const controller = new Controller(logger);
-
-    try {
-        await controller.initialize();
-    } catch (error) {
-        dialog.showMessageBoxSync({ type: "error", message: error.message });
-        app.exit();
-    }
+        try {
+            await controller.initialize();
+        } catch (error) {
+            dialog.showMessageBoxSync({ type: "error", message: error.message });
+            app.exit();
+        }
+    });
 }
